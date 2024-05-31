43fe7b797ff66f2fddf23ed287697beb4dfb1f98
slabiba22 <149197640+slabiba22@users.noreply.github.com>
2024-05-30 21:38:54 -0400

    Half done BST

--- BST.java
+++ BST.java
@@ -35,6 +35,139 @@ public class BST<T extends Comparable<T>> implements Iterable<BSTNode<T>> {
     }
 
 
+    // ----------------------------------------------------------
+    /**
+     * Inserts a given node with the given value
+     * 
+     * @param e
+     *            the value to be inserted
+     */
+    public void insert(T e) {
+        root = insertHelper(e, root);
+        size++;
+    }
+    
+ // ----------------------------------------------------------
+    /**
+     * Remove the specified value from the tree.
+     *
+     * @param x
+     *            the item to remove
+     */
+    public void remove(T x)
+    {
+        root = removeHelper(root, x);
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Private method to insert a value into a subtree.
+     *
+     * @param x
+     *            the item to insert.
+     * @param node
+     *            the node that roots the subtree.
+     * @return the new root of the subtree.
+     */
+    private BSTNode<T> insertHelper(T x, BSTNode<T> node) {
+        if (node == null) {
+            return new BSTNode<T>(x);
+        }
+        if (node.getValue().compareTo(x) >= 0) {
+            node.setLeft(insertHelper(x, node.getLeft()));
+        }
+        else {
+            node.setRight(insertHelper(x, node.getRight()));
+        }
+        return node;
+    }
+
+
+    private T findHelper(BSTNode<T> rt, T key) {
+        if (rt == null) {
+            return null;
+        }
+        if (rt.getValue().compareTo(key) > 0) {
+            return findHelper(rt.getLeft(), key);
+        }
+        else if (rt.getValue().compareTo(key) == 0) {
+            return rt.getValue();
+        }
+        else {
+            return findHelper(rt.getRight(), key);
+        }
+    }
+
+
+    private BSTNode<T> deleteMax(BSTNode<T> rt)
+    {
+        if (rt.getRight() == null)
+        {
+            return rt.getLeft();
+        }
+        rt.setRight(deleteMax(rt.getRight()));
+        return rt;
+        
+    }
+
+
+    /**
+     * Private method to find the largest value in
+     *
+     * @param node
+     *            root node
+     * @return node containing the largest item.
+     */
+    private BSTNode<T> getMax(BSTNode<T> node) {
+        if (node == null) {
+            return null;
+        }
+        else if (node.getRight() == null) {
+            return node;
+        }
+        else {
+            return getMax(node.getRight());
+        }
+    }
+
+
+    private BSTNode<T> removeHelper(BSTNode<T> rt, T key)
+    {
+        if (rt == null)
+        {
+            return null;
+        }
+        if(rt.getValue().compareTo(key) > 0)
+        {
+            rt.setLeft(removeHelper(rt.getLeft(), key));
+        }
+        else if(rt.getValue().compareTo(key) < 0)
+        {
+            rt.setRight(removeHelper(rt.getRight(), key));
+        }
+        else
+        {
+            if(rt.getLeft() == null)
+            {
+                return rt.getRight();
+            }
+            else if(rt.getRight() == null)
+            {
+                return rt.getLeft();
+            }
+            else
+            {
+                BSTNode<T> temp = getMax(rt.getLeft());
+                rt.setValue(temp.getValue());
+                rt.setLeft(deleteMax(rt.getLeft()));
+            }
+            
+        }
+        return rt;
+    }
+
+
     /**
      * This is an auto-generated method stub for an iterator object because we
      * have implemented Iterable to traverse the BST (perhaps during the
--- BSTNode.java
+++ BSTNode.java
@@ -1,3 +1,14 @@
+// -------------------------------------------------------------------------
+/**
+ * Write a one-sentence summary of your class here.
+ * Follow it with additional details about its purpose, what abstraction
+ * it represents, and how to use it.
+ * 
+ * @param <T>
+ * 
+ * @author labibasajjad
+ * @version May 30, 2024
+ */
 public class BSTNode<T extends Comparable<T>> {
     /** The left */
     private BSTNode<T> left;
@@ -14,9 +25,95 @@ public class BSTNode<T extends Comparable<T>> {
      */
     public BSTNode(T value) {
         this.value = value;
+        this.left = null;
+        this.right = null;
+
     }
 
-    // It could be worth including a toString() method for BSTNode for easier
-    // debugging!
+
+    // ----------------------------------------------------------
+    /**
+     * Returns the value store stored in the node
+     * 
+     * @return the value
+     */
+    public T getValue() {
+        return value;
+    }
+    
+ // ----------------------------------------------------------
+    /**
+     * Set the data value stored in this node.
+     * @param value the new data value to set
+     */
+    public void setValue(T value)
+    {
+        this.value = value;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Getter method that returns the left child
+     * 
+     * @return left child node
+     */
+    public BSTNode<T> getLeft() {
+        return left;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Set this node's left child.
+     * 
+     * @param left
+     *            node that points to the left child.
+     */
+    public void setLeft(BSTNode<T> left) {
+        this.left = left;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Getter method that returns the right child
+     * 
+     * @return right child node
+     */
+    public BSTNode<T> getRight() {
+        return right;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Set this node's right child
+     * 
+     * @param right
+     *            node that points ot the right child
+     */
+    public void setRight(BSTNode<T> right) {
+        this.right = right;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Prints the node (in-order)
+     * 
+     * @return a string containing the nodes
+     */
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        if (left != null) {
+            builder.append(left.toString() + ", ");
+        }
+        builder.append(value.toString());
+        if (right != null) {
+            builder.append(", " + right.toString());
+        }
+        return builder.toString();
+    }
 
 }
